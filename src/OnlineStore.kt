1444987d303096d11a1b46527ced5a04e43081ce
Alexandr Samojlov <samojlovalex@gmail.com>
2024-08-29 09:39:01 +0500

    В классе OnlineStore создал свойства класса, инициализировал объекты классов отдельных магазинов, создал список listStore магазинов

--- src/OnlineStore.kt
+++ src/OnlineStore.kt
@@ -1,2 +1,12 @@
-class OnlineStore {
+class OnlineStore (
+    private val name: String,
+    private val assortment: List<String>,
+    private val ufaPrice: List<Double>,
+    private val kazanPrice: List<Double>,
+) {
+
+    private val ufa = UfaStore(assortment, ufaPrice)
+    private val kazan = KazanStore(assortment, kazanPrice)
+
+    private val listStore = listOf(ufa, kazan)
 }
